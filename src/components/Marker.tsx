8d60e6c89b415b86cebcfd2094382e1ad30e8b82
Augustino Simic <augustino.simic@gmail.com>
2023-06-25 19:34:54 +0100

    useRef removed

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { tileX, tileY, gridOriginX } from "../store/definitions";
 
 interface MarkerProps {
@@ -14,51 +14,42 @@ export default function Marker({
   selectedTile,
   onDestination,
 }: MarkerProps) {
-  const isMountRef = useRef(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  const handleRef = () => {
-    isMountRef.current = true;
-  };
-
   useEffect(() => {
-    if (isMountRef.current) {
-      const [row, col] = selectedTile;
-      const centerX = gridOriginX + (tileX / 2) * Math.sin(Math.PI / 4) - 10;
-      const targetX = centerX - (row * tileX) / 2 + (col * tileX) / 2;
-      const targetY = ((row + col) * tileY) / 2 + tileY / 2 - 10;
+    const [row, col] = selectedTile;
+    const centerX = gridOriginX + (tileX / 2) * Math.sin(Math.PI / 4) - 10;
+    const targetX = centerX - (row * tileX) / 2 + (col * tileX) / 2;
+    const targetY = ((row + col) * tileY) / 2 + tileY / 2 - 10;
 
-      const distanceX = targetX - position.x;
-      const distanceY = targetY - position.y;
+    const distanceX = targetX - position.x;
+    const distanceY = targetY - position.y;
 
-      const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
-      const duration = distance / 100; // 100px per second
+    const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
+    const duration = distance / 100; // 100px per second
 
-      const startTime = performance.now();
+    const startTime = performance.now();
 
-      const animationFrame = () => {
-        const elapsed = (performance.now() - startTime) / 1000; // Convert to seconds
+    const animationFrame = () => {
+      const elapsed = (performance.now() - startTime) / 1000; // Convert to seconds
 
-        if (elapsed >= duration) {
-          setPosition({ x: targetX, y: targetY });
-          onDestination(id, row, col);
-        } else {
-          const progress = elapsed / duration;
-          const easeProgress = 0.5 - 0.5 * Math.cos(Math.PI * progress);
+      if (elapsed >= duration) {
+        setPosition({ x: targetX, y: targetY });
+        onDestination(id, row, col);
+      } else {
+        const progress = elapsed / duration;
+        const easeProgress = 0.5 - 0.5 * Math.cos(Math.PI * progress);
 
-          setPosition({
-            x: position.x + distanceX * easeProgress,
-            y: position.y + distanceY * easeProgress,
-          });
+        setPosition({
+          x: position.x + distanceX * easeProgress,
+          y: position.y + distanceY * easeProgress,
+        });
 
-          requestAnimationFrame(animationFrame);
-        }
-      };
+        requestAnimationFrame(animationFrame);
+      }
+    };
 
-      requestAnimationFrame(animationFrame);
-    }
-
-    return handleRef();
+    requestAnimationFrame(animationFrame);
   }, [selectedTile]);
 
   return (
